5090585ef45c49c2c708417fcae3a22be4287d85
ViniciusNyari <viniciusnyari@gmail.com>
2020-05-13 08:40:44 -0300

    Estilos globais

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Title } from './style';
 
 // Criando um componetente do tipo função
+/*
+//Aqui temos um exemplo mais aprimorado
 function Main() {
   return (
     <Title error={false}>
@@ -9,6 +11,11 @@ function Main() {
       <small>Menor</small>
     </Title>
   );
+}
+*/
+
+function Main() {
+  return <Title>Hello World!</Title>;
 }
 
 export default Main;
--- src/pages/Main/style.js
+++ src/pages/Main/style.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+// Esse css se aplica somente ao componente Main
+/*
 export const Title = styled.h1`
   font-size: 24px;
   color: ${(props) => (props.error ? 'red' : '#7159c1')};
@@ -9,4 +11,9 @@ export const Title = styled.h1`
     font-size: 14px;
     color: #333;
   }
+`;
+*/
+
+export const Title = styled.h1`
+  color: #fff;
 `;
