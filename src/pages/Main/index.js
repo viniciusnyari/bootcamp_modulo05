56a5a6922be064a06d24a68a0df7763c8694449b
ViniciusNyari <viniciusnyari@gmail.com>
2020-05-14 09:07:30 -0300

    Estilizando página Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Title } from './style';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './style';
 
 // Criando um componetente do tipo função
 /*
 //Aqui temos um exemplo mais aprimorado
+//Aí tem que importar Title de ./style
 function Main() {
   return (
     <Title error={false}>
@@ -15,7 +17,22 @@ function Main() {
 */
 
 function Main() {
-  return <Title>Hello World!</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar Repositório" />
+
+        <SubmitButton disabled>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 export default Main;
--- src/pages/Main/style.js
+++ src/pages/Main/style.js
@@ -14,6 +14,49 @@ export const Title = styled.h1`
 `;
 */
 
-export const Title = styled.h1`
-  color: #fff;
+export const Container = styled.div`
+  max-width: 700px;
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  padding: 80px;
+  margin: 80px auto;
+
+  h1 {
+    font-size: 20px;
+    display: flex;
+    flex-direction: row;
+    align-items: center; /*display flex, direction row e align center coloca tudo na mesma linha centralizado */
+  }
+
+  svg {
+    margin-right: 10px;
+  }
+`;
+
+export const Form = styled.form`
+  margin-top: 30px;
+  display: flex; /*display: flex com direction row coloca na mesma linha */
+  flex-direction: row;
+
+  input {
+    flex: 1; /* para ocupar todo o espaço*/
+    border: 1px solid #eee;
+    padding: 10px 15px;
+    border-radius: 4px;
+    font-size: 16px;
+  }
+`;
+
+export const SubmitButton = styled.button.attrs({
+  type: 'submit',
+})`
+  background: #7159c1;
+  border: 0;
+  padding: 0 15px;
+  margin-left: 10px;
+  border-radius: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
