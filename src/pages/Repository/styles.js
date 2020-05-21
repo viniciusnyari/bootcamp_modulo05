36443b434eb8e6dbc6ad16570b9274c678046483
ViniciusNyari <viniciusnyari@gmail.com>
2020-05-21 08:17:10 -0300

    Exibindo issues

--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -41,3 +41,63 @@ export const Owner = styled.div`
     max-width: 400px;
   }
 `;
+
+export const IssueList = styled.ul`
+  padding-top: 30px;
+  margin-top: 30px; /** deixa uma borda entre o cnteúdo de cima o conteúdo de baixo */
+  border-top: 1px solid #eee;
+  list-style: none;
+
+  li {
+    display: flex;
+    padding: 15px 10px;
+    border: 1px solid #eee;
+
+    /** estilizar somente da seguna li para frente */
+    & + li {
+      margin-top: 10px;
+    }
+
+    img {
+      width: 36px;
+      height: 36px;
+      border-radius: 50%;
+      border: 2px solid #eee;
+    }
+
+    div {
+      flex: 1; /**ocupar todo espaço possível - evitar que passe do espaço existente*/
+      margin-left: 15px;
+
+      strong {
+        font-size: 16px;
+
+        a {
+          text-decoration: none;
+          color: #333;
+
+          &:hover {
+            color: #7159c1;
+          }
+        }
+
+        span {
+          background: #eee;
+          color: #333;
+          border-radius: 2px;
+          font-size: 12px;
+          font-weight: 600;
+          height: 20px;
+          padding: 3px 4px;
+          marging-left: 10px;
+        }
+      }
+
+      p {
+        margin-top: 5px;
+        font-size: 12px;
+        color: #999;
+      }
+    }
+  }
+`;
