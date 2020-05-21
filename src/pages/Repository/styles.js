f1c05b9c49c39a6dc3ba4803ceb59805dacdbb1f
ViniciusNyari <viniciusnyari@gmail.com>
2020-05-21 08:18:07 -0300

    Exibindo issues

--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -89,7 +89,7 @@ export const IssueList = styled.ul`
           font-weight: 600;
           height: 20px;
           padding: 3px 4px;
-          marging-left: 10px;
+          margin-left: 10px;
         }
       }
 
